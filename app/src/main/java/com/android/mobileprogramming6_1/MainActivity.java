7f91c8aa2f92c0d1acf33426d019f7bfb95a7102
sohee7528 <sohee7528@hanmail.net>
2021-10-14 11:56:39 +0900

    모바일프로그래밍 6주차-대화상자

--- app/src/main/java/com/android/mobileprogramming6_1/MainActivity.java
+++ app/src/main/java/com/android/mobileprogramming6_1/MainActivity.java
@@ -1,8 +1,13 @@
 package com.android.mobileprogramming6_1;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AlertDialog.Builder dlg=new AlertDialog.Builder(MainActivity.this);
+                dlg.setTitle("제목입니다");
+                dlg.setMessage("이곳이 내용입니다");
+                dlg.setIcon(R.mipmap.ic_launcher);dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "확인을 눌렀네요", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                dlg.setCancelable(false);
+                dlg.show();
+            }
+        });
     }
 }
